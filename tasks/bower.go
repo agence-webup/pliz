8c0f2f328804d6d280378f646d941ff819a0ce11
Matthieu Martin <syfonseq@gmail.com>
2016-03-03 23:54:17 +0100

    Utilisation de la config pour le nom du container de build

--- tasks/bower.go
+++ tasks/bower.go
@@ -1,13 +1,20 @@
 package tasks
 
-import "webup/pliz/helpers"
+import (
+	"webup/pliz/config"
+	"webup/pliz/helpers"
+)
 
 // Create a task for running 'npm install'
 func BowerTask() Task {
 	task := Task{Name: "bower", Description: "Run 'bower install' in the build container"}
 
+	// check if 'bower.json' has been updated since last install into 'public/bower'
 	task.ExecutionCheck = &ModificationDateTaskExecutionCheck{UpdatedFile: "bower.json", CompareTo: "public/bower"}
-	task.Command = helpers.NewContainerCommand("srcbuild", []string{"bower", "install", "--allow-root"})
+
+	// execute 'bower install' into the builder container
+	builderContainer := config.Get().Containers.Builder
+	task.Command = helpers.NewContainerCommand(builderContainer, []string{"bower", "install", "--allow-root"})
 
 	return task
 }
--- tasks/npm.go
+++ tasks/npm.go
@@ -1,13 +1,20 @@
 package tasks
 
-import "webup/pliz/helpers"
+import (
+	"webup/pliz/config"
+	"webup/pliz/helpers"
+)
 
 // Create a task for running 'npm install'
 func NpmTask() Task {
 	task := Task{Name: "npm", Description: "Run 'npm install' in the build container"}
 
+	// check if 'package.json' has been updated since last install into 'node_modules'
 	task.ExecutionCheck = &ModificationDateTaskExecutionCheck{UpdatedFile: "package.json", CompareTo: "node_modules"}
-	task.Command = helpers.NewContainerCommand("srcbuild", []string{"npm", "install"})
+
+	// execute 'npm install' into the builder container
+	builderContainer := config.Get().Containers.Builder
+	task.Command = helpers.NewContainerCommand(builderContainer, []string{"npm", "install"})
 
 	return task
 }
