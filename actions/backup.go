3d11beb6ec2f746be21e83f645324645144bed9c
Matthieu Martin <matthieu@agence-webup.com>
2016-05-12 11:47:24 +0200

    Fix bugs sur linux : - les hard links ne sont possibles que sur une même partition -> on crée le dossier tmp dans le dossier courant - les directories ne peuvent pas être hard linkés -> on fait un walk et on link les fichiers seulement

--- actions/backup.go
+++ actions/backup.go
@@ -36,7 +36,8 @@ func BackupActionHandler(ctx domain.ExecutionContext, backupFilesOpt *bool, back
 	fmt.Println("")
 
 	// prepare the directory to store the backup
-	backupDir, err := ioutil.TempDir("", "plizbackup")
+	backupDir := ".pliz_backup"
+	err := os.Mkdir(backupDir, os.ModePerm)
 	if err != nil {
 		fmt.Printf("Unable to create a backup directory: %s\n", err)
 		return
@@ -47,9 +48,9 @@ func BackupActionHandler(ctx domain.ExecutionContext, backupFilesOpt *bool, back
 		dir := path.Join(backupDir, "backup", "config")
 		os.MkdirAll(dir, 0755)
 		for _, configFile := range config.Get().ConfigFiles {
-			if _, err := os.Stat(configFile.Target); err == nil {
+			if _, err = os.Stat(configFile.Target); err == nil {
 				target := path.Join(dir, configFile.Target)
-				os.MkdirAll(filepath.Dir(target), 0755)
+				os.MkdirAll(filepath.Dir(target), os.ModePerm)
 				os.Link(configFile.Target, target)
 			} else {
 				fmt.Println(err)
@@ -73,18 +74,30 @@ func BackupActionHandler(ctx domain.ExecutionContext, backupFilesOpt *bool, back
 	if backupFiles {
 		// files
 		filesDir := path.Join(backupDir, "backup", "files")
-		os.MkdirAll(filesDir, 0755)
+		os.MkdirAll(filesDir, os.ModePerm)
+
+		// prepare a walk function to handle the whole hierarchy
+		walkFunc := func(filepath string, info os.FileInfo, err error) error {
+			target := path.Join(filesDir, filepath)
+
+			// just create the directory
+			if info.IsDir() {
+				err := os.MkdirAll(target, os.ModePerm)
+				return err
+			}
+
+			// perform a hard link
+			linkErr := os.Link(filepath, target)
+			return linkErr
+		}
+
 		for _, file := range config.Get().BackupConfig.Files {
-			target := path.Join(filesDir, file)
-			os.MkdirAll(filepath.Dir(target), 0755)
-			os.Link(file, target)
+			err := filepath.Walk(file, walkFunc)
+			if err != nil {
+				fmt.Printf("Unable to walk into %s\n%s\n", file, err)
+			}
 		}
 	}
-
-	// if err := cmd.WriteResultToFile(file); err != nil {
-	// 	os.Remove(file.Name())
-	// 	return err
-	// }
 
 	tar := new(archivex.TarFile)
 	tar.Create(path.Join(backupDir, "backup_archive.tar.gz"))
@@ -101,10 +114,17 @@ func BackupActionHandler(ctx domain.ExecutionContext, backupFilesOpt *bool, back
 		hour, minutes, seconds := now.Clock()
 		archiveFilename = fmt.Sprintf("backup-%d%02d%02d_%02d%02d%02d.tar.gz", year, month, day, hour, minutes, seconds)
 	}
-	os.Rename(path.Join(backupDir, "backup_archive.tar.gz"), archiveFilename)
+
+	err = os.Rename(path.Join(backupDir, "backup_archive.tar.gz"), archiveFilename)
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	// clean tmp
-	os.RemoveAll(backupDir)
+	err = os.RemoveAll(backupDir)
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	fmt.Printf("\n %s Done\n", color.GreenString("✓"))
 }
@@ -135,12 +155,12 @@ func makeDump(ctx domain.ExecutionContext, dbBackup domain.DatabaseBackupConfig,
 	}
 
 	if dbType == "mysql" {
-		err := mysqlDump(path.Join(backupDir, "dump.sql"), containerID, config.Env)
+		err := mysqlDump(path.Join(backupDir, "dump.sql"), containerID, config.Env, backupDir)
 		if err != nil {
 			fmt.Println(err)
 		}
 	} else if dbType == "mongo" {
-		err := mongoDump(path.Join(backupDir, "mongodb.archive"), containerID, config.Env)
+		err := mongoDump(path.Join(backupDir, "mongodb.archive"), containerID, config.Env, backupDir)
 		if err != nil {
 			fmt.Println(err)
 		}
@@ -150,7 +170,7 @@ func makeDump(ctx domain.ExecutionContext, dbBackup domain.DatabaseBackupConfig,
 
 }
 
-func mysqlDump(destination string, containerId string, env domain.DockerContainerEnv) error {
+func mysqlDump(destination string, containerId string, env domain.DockerContainerEnv, backupDir string) error {
 
 	password := ""
 	if value, ok := env["MYSQL_ROOT_PASSWORD"]; ok {
@@ -164,7 +184,7 @@ func mysqlDump(destination string, containerId string, env domain.DockerContaine
 
 	cmd := domain.NewCommand([]string{"docker", "exec", "-i", containerId, "mysqldump", fmt.Sprintf("--password=%s", password), database})
 
-	file, err := ioutil.TempFile("", "plizdump")
+	file, err := ioutil.TempFile(backupDir, "plizdump")
 	if err != nil {
 		fmt.Println("Unable to create a tmp file:")
 		return err
@@ -181,11 +201,11 @@ func mysqlDump(destination string, containerId string, env domain.DockerContaine
 	return nil
 }
 
-func mongoDump(destination string, containerId string, env domain.DockerContainerEnv) error {
+func mongoDump(destination string, containerId string, env domain.DockerContainerEnv, backupDir string) error {
 
 	cmd := domain.NewCommand([]string{"docker", "exec", "-i", containerId, "mongodump", "--archive", "--gzip"})
 
-	file, err := ioutil.TempFile("", "plizdump")
+	file, err := ioutil.TempFile(backupDir, "plizdump")
 	if err != nil {
 		fmt.Println("Unable to create a tmp file:")
 		return err
