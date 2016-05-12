0e413d95d09ab3f90f242c775c2e83f02a6d43e5
Matthieu Martin <matthieu@agence-webup.com>
2016-05-12 11:47:30 +0200

    Build 12

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ func main() {
 
 	app := cli.App("pliz", "Manage projects building")
 
-	app.Version("v version", "Pliz 3 dev (build 11 WIP)")
+	app.Version("v version", "Pliz 3 (build 12)")
 
 	// option to change the Pliz env
 	plizEnv := app.String(cli.StringOpt{
