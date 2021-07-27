ae0b3ca67405f5c97c0f63fa72686575e5262b6f
Baptiste Ducatel <baptiste@agence-webup.com>
2021-07-27 14:50:51 +0200

    v10

--- main.go
+++ main.go
@@ -25,7 +25,7 @@ func main() {
 
 	app := cli.App("pliz", "Manage projects building")
 
-	app.Version("v version", "Pliz 9")
+	app.Version("v version", "Pliz 10")
 
 	// option to change the Pliz env
 	plizEnv := app.String(cli.StringOpt{
